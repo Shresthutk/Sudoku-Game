3f4ef9264c19be906f956b9b585e32103a1e98f3
Utkarsh Shresth <shresthutk62@gmail.com>
2021-12-03 18:53:22 +0530

    sudokusolver file modified

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,31 +1,31 @@
-def solve(bo):
-    find = find_empty(bo)
+def solve(sudoku):
+    find = find_empty(sudoku)
     if not find:
         return True
     else:
         row, col = find
 
     for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+        if valid(sudoku, i, (row, col)):
+            sudoku[row][col] = i
 
-            if solve(bo):
+            if solve(sudoku):
                 return True
 
-            bo[row][col] = 0
+            sudoku[row][col] = 0
 
     return False
 
 
-def valid(bo, num, pos):
+def valid(sudoku, num, pos):
     # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+    for i in range(len(sudoku[0])):
+        if sudoku[pos[0]][i] == num and pos[1] != i:
             return False
 
     # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
+    for i in range(len(sudoku)):
+        if sudoku[i][pos[1]] == num and pos[0] != i:
             return False
 
     # Check box
@@ -34,15 +34,15 @@ def valid(bo, num, pos):
 
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+            if sudoku[i][j] == num and (i,j) != pos:
                 return False
 
     return True
 
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
+def find_empty(sudoku):
+    for i in range(len(sudoku)):
+        for j in range(len(sudoku[0])):
+            if sudoku[i][j] == 0:
                 return (i, j)  # row, col
 
     return None
